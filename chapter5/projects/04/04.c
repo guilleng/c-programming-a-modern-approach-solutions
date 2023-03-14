d63c15c57f6483d93bd766a04852fc95ad002378
ffstlln <gngaray@gmail.com>
2023-03-14 10:12:53 -0300

    review and refactoring

--- chapter5/projects/04/04.c
+++ chapter5/projects/04/04.c
@@ -9,28 +9,28 @@
 
 int main(void)
 {
-    float wind_speed;
+    int wind_speed;
 
     printf("Enter a wind speed (in knots): ");
     scanf("%f", &wind_speed);
 
-    if (wind_speed <= 1.0f)
+    if (wind_speed < 1)
     {
         printf("Calm\n");
     }
-    else if (wind_speed <= 3.0f)
+    else if (wind_speed < 4)
     {
         printf("Light air\n");
     }
-    else if (wind_speed <= 27.0f)
+    else if (wind_speed < 28)
     {
         printf("Breeze\n");
     }
-    else if (wind_speed <= 47.0f)
+    else if (wind_speed < 48)
     {
         printf("Gale\n");
     }
-    else if (wind_speed <= 63.0f)
+    else if (wind_speed < 64)
     {
         printf("Storm\n");
     }
