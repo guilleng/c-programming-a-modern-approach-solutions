f49a6fca3c4ad28bac0d66473b9cda129c5ebab7
ffstlln <gngaray@gmail.com>
2023-04-30 12:22:35 -0300

    completed project 06

--- chapter16/projects/06/dates.c
+++ chapter16/projects/06/dates.c
@@ -10,20 +10,32 @@
 #include "user_input.h"
 #define BUF_LEN 10
 
+typedef struct {
+    int month;
+    int day;
+    int year;
+} Dates;
+
 /*
  * Allocates a buffer for an user inputted date, validates it and stores it in 
  * the pointer arguments.
  */
 void get_user_date(int *mm, int *dd, int *yy);
 
+/*
+ * Returns the day of the year (an integer between 1 and 366) that corresponds 
+ * to the date d.
+ */
+int day_of_year(Dates d);
+
+/*
+ * Returns -1 if d1 is an earlier date than d2, +1 if d1 is a later date than
+ * d2, and 0 if d1 and d2 are the same.
+ */
+int compare_dates(Dates d1, Dates d2);
+
 int main(void)
 {
-    typedef struct {
-        int month;
-        int day;
-        int year;
-    } Dates;
-
     Dates d1, d2;
 
     printf("Enter first date (mm/dd/yyyy): ");
@@ -32,21 +44,23 @@ int main(void)
     printf("Enter second date (mm/dd/yyyy): ");
     get_user_date(&d2.month, &d2.day, &d2.year);
 
-    if ((d1.year < d2.year) || (d1.month < d2.month) || (d1.day < d2.day))
+    switch (compare_dates(d1, d2))
     {
-        printf("%.2d/%.2d/%.4d is earlier than %.2d/%.2d/%.4d\n", 
-                d1.month, d1.day, d1.year, 
-                d2.month, d2.day, d2.year);
-    }
-    else if ((d2.year < d1.year) || (d2.month < d1.month) || (d2.day < d1.day))
-    {
-        printf("%.2d/%.2d/%.4d is earlier than %.2d/%.2d/%.4d\n", 
-                d2.month, d2.day, d2.year, 
-                d1.month, d2.day, d2.year);
-    }
-    else
-    {
-        printf("They are the same date\n");
+        case -1:
+            printf("%.2d/%.2d/%.4d is earlier than %.2d/%.2d/%.4d\n", 
+            d1.month, d1.day, d1.year, 
+            d2.month, d2.day, d2.year);
+            break;
+        case 1:
+            printf("%.2d/%.2d/%.4d is earlier than %.2d/%.2d/%.4d\n", 
+            d2.month, d2.day, d2.year, 
+            d1.month, d2.day, d2.year);
+            break;
+        case 0:
+            printf("They are the same date\n");
+            break;
+        default:
+            break;
     }
 
     exit(EXIT_SUCCESS);
@@ -68,4 +82,51 @@ void get_user_date(int *mm, int *dd, int *yy)
     }
 
     return; 
+}
+
+int day_of_year(Dates d)
+{
+    int i, day_of_year = 0;
+    int days_in_month[12] = { 31, 28, 31, 30,                                   
+                           /* Jan Feb Mar Apr */
+                              31, 30, 31, 31,                                   
+                          /* May Jun Jul Aug */
+                              30, 31, 30, 31 };
+                          /* Set Oct Nov Dec */
+                                                
+    if ( (d.year % 4 == 0 && d.year % 100 != 0) || d.year % 400 == 0 )
+    {
+        days_in_month[1]++;
+    }
+
+    for (i = 0; i < (d.month - 1); i++)
+    {
+        day_of_year += days_in_month[i];
+    }
+
+    return day_of_year + d.day;
+}
+
+int compare_dates(Dates d1, Dates d2)
+{
+	if (d1.year < d2.year)
+	{   
+		return -1;
+	}
+	else if (d2.year < d1.year)
+	{   
+		return 1;
+	}
+    else if (day_of_year(d1) < day_of_year(d2))
+    {
+        return -1;
+    }
+    else if (day_of_year(d1) > day_of_year(d2))
+    {
+        return 1;
+    }
+    else 
+    {
+        return 0;
+    }
 }
